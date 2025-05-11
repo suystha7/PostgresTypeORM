91918c8648cd8ecc2b78a01f90f7899504f9e329
suyogstha7 <suyogstha317@gmail.com>
2025-05-11 22:06:41 +0545

    entites created for user

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,46 @@
 import "reflect-metadata";
 import express from "express";
 import { DataSource } from "typeorm";
+import { User } from "./entites/User";
 
 const app = express();
 app.use(express.json());
 
 const port = 3000;
+
+app.get("/", async (req, res) => {
+  const userRepo = AppDataSource.getRepository(User);
+
+  // find all the records
+  // const allRecords = await userRepo.find();
+  // res.json(allRecords);
+
+  //delete
+  //  await userRepo.delete(2)
+  // res.send("Record Deleted");
+
+  // insert
+  // let user: User = new User();
+  // (user.email = "suyogstha317@gmail.com"),
+  //   (user.firstName = "Suyog"),
+  //   (user.lastName = "Shrestha");
+
+  // const userInsert = await userRepo.save(user);
+
+  // update
+  // await userRepo.update(8, {
+  //   firstName: "Sumana",
+  //   lastName: "Shakya",
+  //   email: "sumanashky@gmail.com",
+  // });
+
+  // res.send("Record Updated");
+
+  // filter
+  const record = await userRepo.findOne({ where: { firstName: "Sumana" } });
+
+  res.json(record)
+});
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -27,7 +62,3 @@ AppDataSource.initialize()
     });
   })
   .catch((err) => console.log("Error Connecting database", err));
-
-app.get("/", (req, res) => {
-  res.send("Hello Suyog");
-});
