17c1cde93402c99c2a40b4d269d7db88cfdb14e4
suyogstha7 <suyogstha317@gmail.com>
2025-05-25 16:46:01 +0545

    db config

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,13 +1,15 @@
 import path from "path";
 import { DataSource } from "typeorm";
+import {config} from "dotenv";
+config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "12345",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [path.join(__dirname, "..", "entites/*.entity{.ts,.js}")],
   synchronize: true,
   logging: true,
